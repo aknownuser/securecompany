5e7a38ed05528523beba3ccc632992bebd1774d4
aNewUser <oussamaelazizi.opt@gmail.com>
2025-02-06 00:09:59 +0100

    add snapshot attachment and set availability zones

--- terraform/aws/modules/response/isolate_ec2/main.tf
+++ terraform/aws/modules/response/isolate_ec2/main.tf
@@ -37,3 +37,39 @@ resource "aws_ssm_activation" "run_command" {
   iam_role = aws_iam_role.run_command_role.id
   depends_on = [ aws_iam_role_policy_attachment.run_command_attach ]
 }
+
+
+
+// Snapshot the EBS volume of infected EC2 instance
+resource "aws_ebs_snapshot" "snapshot" {
+  volume_id = var.target_ebs_volume_id
+  tags = {
+    Name = "Snapshot of infected EC2 instance"
+  }
+}
+// Create a new EBS volume from the snapshot
+resource "aws_ebs_volume" "target" {
+  availability_zone = var.availability_zone
+  size = 1
+  snapshot_id = aws_ebs_snapshot.snapshot.id
+  tags = {
+    Name = "Snapshot of infected EC2 instance"
+  }
+  
+}
+# Create host with forensics AMI
+resource "aws_instance" "forensics" {
+    ami = var.forensics_ami_id
+    availability_zone = var.availability_zone
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.isolated.id
+    
+}
+# attach created volume to the forensics instance
+resource "aws_volume_attachment" "forensics" {
+    device_name = "/dev/sdf"
+    volume_id = aws_ebs_volume.target.id
+    instance_id = aws_instance.forensics.id
+  
+}
+
--- terraform/aws/modules/response/isolate_ec2/variables.tf
+++ terraform/aws/modules/response/isolate_ec2/variables.tf
@@ -1,5 +1,23 @@
 variable "region" {
-    description = "The path to the policy to be used in the key"
+    description = "Region name"
     type = string
     default = "eu-south-2"
+}
+
+variable target_ebs_volume_id {
+    description = "EBS Volume ID to a take a snapshot of "
+    type = string
+}
+
+variable "forensics_ami_id" {
+    description = "AMI ID for the forensics instance"
+    type = string
+  
+}
+
+variable "availability_zone" {
+    description = "Availability zone"
+    type = string
+    default = "eu-south-2a"
+  
 }
